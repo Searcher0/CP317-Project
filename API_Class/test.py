1ad7089bde9bf977845a75513355f5045962efbe
Searcher0 <muneebzaidi0@gmail.com>
2024-05-31 18:06:26 -0400

    Saves content to JSON file. Properly gets content

--- API_Class/test.py
+++ API_Class/test.py
@@ -1,1 +1,15 @@
-print("HELLO")
+import requests
+
+# URL to scrape
+url = "https://www.nofrills.ca/food/fruits-vegetables/fresh-vegetables/c/28195?navid=flyout-L3-Fresh-Vegetables"
+
+# Fetch HTML content
+response = requests.get(url)
+html_content = response.text
+
+# Save HTML content to a file
+file_path = "C:/CP317 Project/CP317-Project/test_1.txt"
+with open(file_path, "w", encoding="utf-8") as file:
+    file.write(html_content)
+
+print(f"HTML content saved to {file_path}")
