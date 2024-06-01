f1702b78bf5c2ec47836a54daa4cdbd6f3df76fe
Searcher0 <muneebzaidi0@gmail.com>
2024-05-31 23:29:17 -0400

    Added Find_Duplicates Script
    This script will read the Json file & sift through the file. Product_Key is made up of (name, price) & we two arrays that are filled as new & duplicate thereby storing any duplicate into the duplicates file. BUT it does not remove the duplicate from original file only shows which are duplicates.

---
+++ API_Class/Find_Duplicates.py
@@ -0,0 +1,28 @@
+import json
+from collections import defaultdict
+
+# Path to the JSON file with products
+input_file = r'C:\CP317 Project\CP317-Project\FreshVeg Complete.json'
+# Path to the JSON file to save duplicates
+output_file = r'C:\CP317 Project\CP317-Project\duplicates.json'
+
+# Read the products from the JSON file
+with open(input_file, 'r') as json_file:
+    products = json.load(json_file)
+
+# Find duplicates
+seen_products = set()
+duplicates = []
+
+for product in products:
+    product_key = (product['name'], product['price'])
+    if product_key in seen_products:
+        duplicates.append(product)
+        print(product_key)
+    else:
+        seen_products.add(product_key)
+
+with open(output_file, 'w') as json_file:
+    json.dump(duplicates, json_file, indent=4)
+
+print("Duplicates have been written to JSON file.")
