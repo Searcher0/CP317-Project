945ebf2d30ac45d831d45ed8e905c90f183711c8
Searcher0 <muneebzaidi0@gmail.com>
2024-06-02 01:27:48 -0400

    Creating all folders
    In this iteration all folders were made & blank text files for the HTML content were created.
    
    New script that makes folders & a file was made for ease of use.

--- API_Class/Create_Folder_&_File.py
+++ API_Class/Create_Folder_&_File.py
@@ -1,11 +1,9 @@
 import os
 
 def create_folders_and_files(base_path, list_file_path):
-    # Read the list of folder names from the file
     with open(list_file_path, 'r') as file:
         folder_names = [line.strip() for line in file]
 
-    # Create folders and files
     for folder_name in folder_names:
         # Strip leading and trailing spaces from folder names
         folder_name = folder_name.strip()
@@ -20,9 +18,7 @@ def create_folders_and_files(base_path, list_file_path):
         with open(file_path, 'w') as file:
             file.write('')  # Create an empty file
 
-# Define the base path and the path to the file containing folder names
-base_path = r'C:\Users\User\OneDrive\Documents\GitHub\CP317-Project\API_Class\HTML_FILES\Fruits & Vegetables'
-list_file_path = r'C:\Users\User\OneDrive\Documents\GitHub\CP317-Project\API_Class\Fruits&Veg.txt'  # Update this to the path of your list file
+base_path = r'C:\Users\User\OneDrive\Documents\GitHub\CP317-Project\API_Class\HTML_FILES\Snacks, Chips & Candy'
+list_file_path = r'C:\Users\User\OneDrive\Documents\GitHub\CP317-Project\API_Class\Text Files\Snacks, Chips & Candy.txt'
 
-# Call the function to create folders and files
 create_folders_and_files(base_path, list_file_path)
